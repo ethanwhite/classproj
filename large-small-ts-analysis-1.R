1e24ac8e12eb8aca67acb60608f99c8f0c1d5e1b
Ethan White <ethan@weecology.org>
2015-10-26 15:59:04 -0400

    Add new analysis file

---
+++ large-small-ts-analysis-1.R
@@ -0,0 +1,14 @@
+get_data <- function(){
+  data <- read.csv('surveys.csv')
+  return(data)
+}
+
+get_size_class <- function(weight, threshold){
+  if (weight > threshold){
+    size_class = 'large'
+  }
+  else{
+    size_class = 'small'
+  }
+  return(size_class)
+}
